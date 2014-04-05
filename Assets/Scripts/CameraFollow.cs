10f13e5e0f03eb6b36dcc8f10ed3b5aaf7ccb2a5
Matthew Adler <madler72@gmail.com>
2014-04-05 19:34:00 -0400

    More Duckling AI... Need to organize the code more and play test it some.
    Now need to make the levels
    Also want to add some AI idle behaviours (spawn little hearts when they're idle for long enough)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject toFollow;
+	private float origMaxSpeed = 5.0f;
+	private float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+		currentSpeed = origMaxSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate()
+	{
+		//Get rid of the normalized here to move fast enough to follow dah player
+		transform.position += new Vector3(((toFollow.transform.position - transform.position).x * currentSpeed * Time.deltaTime), ((toFollow.transform.position - transform.position).y * currentSpeed * Time.deltaTime), 0);
+	}
+}
--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -6,14 +6,29 @@ public class Follow : MonoBehaviour {
 	public GameObject toFollow;
 
 	private float origMaxSpeed = 5.0f;
-	private float maxSpeed;
+	private float currentSpeed;
 	//public float moveForce = 365f;	
 	private float jumpForce = 280;
 	private float followPtDisplace;
 	private Vector3 actualFollowPoint;
+
 	private bool wantsToJump;
+	private bool following;
+	private bool isIdle;
+	private bool isAnnealing;
+	private bool overGap;
+	private bool seriousGapBuisinessTime;
+
 	private float jumpWaitTime;
 	private float timeOfJump;
+	private float idleWaitTime = 2.0f; //5 seconds
+	private float timeOfLastMove;
+
+	private float annealingTime = 1.0f;
+	private float updateWhileNotAnnealing;
+
+
+	private float prevX;
 
 
 	public bool grounded;
@@ -24,22 +39,62 @@ public class Follow : MonoBehaviour {
 		followPtDisplace = Random.Range(-0.5f, 0.5f);
 		actualFollowPoint = new Vector3(toFollow.transform.position.x + followPtDisplace, toFollow.transform.position.y, toFollow.transform.position.z);
 		jumpWaitTime = Random.Range(0.1f, 2f);
+		jumpForce += Random.Range(-10, 5);
+		following = true;
+		isIdle = true;
+		isAnnealing = false;
+		overGap = false;
+		seriousGapBuisinessTime = false;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		//DO ALWAYS
 
 		//Randomly jumping cuz is cute
 		if (Time.time - timeOfJump > jumpWaitTime)
 		{
 			wantsToJump = true;
-			jumpWaitTime = Random.Range(0.1f, 2f);
+			jumpWaitTime = Random.Range(0.3f, 2f);
 		}
 
-		actualFollowPoint = new Vector3(toFollow.transform.position.x + followPtDisplace, toFollow.transform.position.y, toFollow.transform.position.z);
 
-		maxSpeed = Mathf.Min(Mathf.Abs(actualFollowPoint.x  -  transform.position.x) * 2.5f, origMaxSpeed);
+		//Follow Point and Speed Calculations
+		actualFollowPoint = new Vector3(toFollow.transform.position.x + followPtDisplace, toFollow.transform.position.y, toFollow.transform.position.z);
+		currentSpeed = Mathf.Min(Mathf.Abs(actualFollowPoint.x  -  transform.position.x) * 2.5f, origMaxSpeed);
+
+
+
+		//Checking if Idle
+
+		//If we're moving fast enough then update our last move time
+		if (Mathf.Abs(prevX - transform.position.x) > 0.02f)
+		{
+			timeOfLastMove = Time.time;
+		}
+	
+		//If it's been too long since a detectable move then we're in Idle mode!
+		if (Time.time - timeOfLastMove > idleWaitTime)
+		{
+			isIdle = true;
+			Debug.Log("Is IDLE");
+		}
+		else
+		{
+			isIdle = false;
+		}
+
+		//Setting the previous x position
+		prevX = transform.position.x;
+
+
+		if (transform.position.y < -50)
+		{
+			Debug.Log("Die");
+			Destroy(gameObject);
+		}
 
 		//maxSpeed = Mathf.Min(Mathf.Abs(toFollow.transform.position.x  -  transform.position.x) * 2.5f, origMaxSpeed);
 
@@ -53,55 +108,154 @@ public class Follow : MonoBehaviour {
 
 
 		//things to do while we're in the air
+		//Always if not grounded float downwards slightly
 		if (!grounded)
 		{
-			//float downwards
-			rigidbody2D.AddForce(new Vector2(0f, jumpForce * 0.01f));
-			
-			//Always be checking if we're grounded
-			grounded = Physics2D.Raycast(transform.position - (new Vector3 (0, transform.localScale.y / 1.99f, 0)) , -Vector2.up, 0.02f, 1 << 10)  //layer mask so only jump on ground
-				|| Physics2D.Raycast(transform.position - (new Vector3 (-(transform.localScale.x / 2.5f), transform.localScale.y / 1.99f, 0)) , -Vector2.up, 0.02f, 1 << 10)
-					|| Physics2D.Raycast(transform.position - (new Vector3 ((transform.localScale.x / 2.5f), transform.localScale.y / 1.99f, 0)) , -Vector2.up, 0.02f, 1 << 10);
+			GlideDown();
+			checkIsGrounded();
 		}
 
-	//IF FOLLOWING~~~~
-	//~~~~~~~~~~~~~~~~
-		//Normalized so that speed is based on only the maximum speed
-		//transform.position += new Vector3(((toFollow.transform.position - transform.position).normalized.x * maxSpeed * Time.deltaTime), 0, 0);
-
-		//X following (movement)
-
-		//If this doesn't change in a long enough time (Probably actually a very short time) 
-		//stop following and try your own thing (if fay away then move in the opposite direction for some time and then keep following)
-		//Otherwise if close enough do some cute stuff (spawn hearts?)
-		transform.position += new Vector3(((actualFollowPoint - transform.position).normalized.x * maxSpeed * Time.deltaTime), 0, 0);
-
-		//Jump!
-		if((toFollow.transform.position.y - transform.position.y  > 1.0f) && grounded)
+		//If we're idle and far enough away try to move bakcwards a bit and then go back to following.
+		if(isIdle && (actualFollowPoint - transform.position).magnitude > 2 && !isAnnealing)
 		{
-			// Add a vertical force to the player.
-			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-			grounded = false; //so that we only jump once
-			wantsToJump = false;
-			timeOfJump = Time.time;
-			Debug.Log("JUMPING");
-			//Maybe put the follow on the player in the air. When grounded return it to behind.
-			
-			//jump = false;
+
+			following = false;
+			isAnnealing = true;
+			annealingTime = Random.Range(0.3f, 1.3f);
+			Debug.Log("WHERE'S MOMMA FREAK OUT!");
 		}
-	
-		if ((wantsToJump) && grounded)
+
+		//If parent is close enough stop annealing and just 
+		if ((actualFollowPoint - transform.position).magnitude < 1)
 		{
-			rigidbody2D.AddForce(new Vector2(0f, Random.Range(150, 200)));
-			grounded = false; //so that we only jump once
-			wantsToJump = false;
-			timeOfJump = Time.time;
-			Debug.Log("JUMPING");
+			seriousGapBuisinessTime = false;
+
+			if (!following)
+			{
+				following = true;
+				isAnnealing = false;
+				//Move a little faster the first frame that we meet the parent
+				FollowPoint();
+				FollowPoint();
+			}
 		}
-	//~~~~~~~~~~~~~~~
 
 
-		//Always if not grounded float downwards slightly
+		//IF FOLLOWING~~~~
+		//~~~~~~~~~~~~~~~~
+		if (following)
+		{
+			isAnnealing = false;
+
+			FollowPoint();
+		//	Debug.Log("FOLLOWING");
+
+			//Jump!
+			if((toFollow.transform.position.y - transform.position.y  > 1.0f) && grounded && !seriousGapBuisinessTime)// && isIdle)
+			{
+				Jump(jumpForce);
+				Debug.Log("JUMPING, trying");
+			}
+			
+			if ((wantsToJump) && grounded && !seriousGapBuisinessTime)
+			{
+				Jump(Random.Range(150, 190));
+				Debug.Log("JUMPING, cute");
+			}
+		}
+
+		//Can't get to parent, so try going back a second and then continue following!
+		if (isAnnealing && !following)
+		{
+			//while we should be running away run away
+			if (Time.time - updateWhileNotAnnealing < annealingTime)
+			{
+				Debug.Log("SHOULD BE RUNNING AWAY");
+				//Minus equals so that running away.
+				RunFromPoint();
+
+			}
+			else
+			{
+				Debug.Log("~~~~~~~RUNNING ELSE~~~~~~~");
+				//Jump(jumpForce);
+				isAnnealing = false;
+				following = true;
+				updateWhileNotAnnealing = Time.time;
+			}
+		}
+		else if (!isAnnealing)
+		{
+			updateWhileNotAnnealing = Time.time;
+		}
+		//~~~~~~~~~~~~~~~
+
+
+
+		//Now we should do smart jumping
+		//Have two raycasts, if either one returns false try jumping at full power.
+		//If not grounded then stop following, anneal and then move in the direction again for one jump
+		//While in the air here never anneal!
+		//If we don't make it then that sucks... :'(
+		overGap = !(Physics2D.Raycast(transform.position - (new Vector3 (-(transform.localScale.x / 2f), transform.localScale.y / 1.99f, 0)) , -Vector2.up, 4, 1 << 10)
+			&& Physics2D.Raycast(transform.position - (new Vector3 ((transform.localScale.x / 2f), transform.localScale.y / 1.99f, 0)) , -Vector2.up, 4, 1 << 10));
+
+		if (overGap && grounded)
+		{
+			Debug.Log("Trying to jump the gap");
+			//seriousGapBuisinessTime = false;
+			Jump(jumpForce);
+		}
+		else if(overGap && !grounded && !seriousGapBuisinessTime)
+		{
+			annealingTime = Random.Range(0.8f, 1.0f);
+			following = false;
+			isAnnealing = true;
+			seriousGapBuisinessTime = true;
+		}
 
 	}
+
+
+	private void Jump(float jumpForce)
+	{
+		//Double check to see if we're grounded
+		if (grounded)
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+		grounded = false; //so that we only jump once
+		wantsToJump = false;
+		timeOfJump = Time.time;
+	}
+
+	private void GlideDown()
+	{
+		//float downwards
+		rigidbody2D.AddForce(new Vector2(0f, jumpForce * 0.01f));
+	}
+
+	private void checkIsGrounded()
+	{
+		//Always be checking if we're grounded
+		grounded = Physics2D.Raycast(transform.position - (new Vector3 (0, transform.localScale.y / 1.99f, 0)) , -Vector2.up, 0.02f, 1 << 10)  //layer mask so only jump on ground
+			|| Physics2D.Raycast(transform.position - (new Vector3 (-(transform.localScale.x / 2.5f), transform.localScale.y / 1.99f, 0)) , -Vector2.up, 0.02f, 1 << 10)
+				|| Physics2D.Raycast(transform.position - (new Vector3 ((transform.localScale.x / 2.5f), transform.localScale.y / 1.99f, 0)) , -Vector2.up, 0.02f, 1 << 10);
+	}
+
+
+	//Normalized so that speed is based on only the maximum speed
+	//transform.position += new Vector3(((toFollow.transform.position - transform.position).normalized.x * maxSpeed * Time.deltaTime), 0, 0);
+	
+	//X following (movement)
+	
+	//If this doesn't change in a long enough time (Probably actually a very short time) 
+	//stop following and try your own thing (if fay away then move in the opposite direction for some time and then keep following)
+	//Otherwise if close enough do some cute stuff (spawn hearts?)
+	private void FollowPoint()
+	{
+		transform.position += new Vector3(((actualFollowPoint - transform.position).normalized.x * currentSpeed * Time.deltaTime), 0, 0);
+	}
+	private void RunFromPoint()
+	{
+		transform.position -= new Vector3(((actualFollowPoint - transform.position).normalized.x * currentSpeed * Time.deltaTime), 0, 0);
+	}	
 }
